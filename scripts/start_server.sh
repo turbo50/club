0a1df3c2e6c9dc3709680da6f047f0fe0c0a0126
Aquilas <aquilas-damico.lengue-lengue-@externe.grdf.fr>
2025-08-12 13:54:51 +0200

    MàJ script start server

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
+set -e
 # Démarrer Apache et l'activer au démarrage
 systemctl start httpd
 systemctl enable httpd
 sudo cp -r /home/ec2-user/club/* /var/www/html/
-
-
-#!/bin/bash
-# Arrêt du script si une commande échoue
-set -e
-
-echo "===== Copie des fichiers de l'application ====="
-sudo cp -r * /var/www/html/
-
 echo "===== Attribution des permissions ====="
 sudo chown -R apache:apache /var/www/html
 sudo chmod -R 755 /var/www/html
