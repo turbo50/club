a2a83762b101a5823e9eb0819b827821d9bce37c
Aquilas <aquilas-damico.lengue-lengue-@externe.grdf.fr>
2025-08-12 14:32:20 +0200

    MàJ scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
-# Démarrer Apache et l'activer au démarrage
-systemctl start httpd
-systemctl enable httpd
-sudo cp -r /home/ec2-user/club/* /var/www/html/
-echo "===== Attribution des permissions ====="
-sudo chown -R apache:apache /var/www/html
-sudo chmod -R 755 /var/www/html
+echo "Installation des dépendances et dézippage du projet"
+
+# Installer unzip si nécessaire
+if ! command -v unzip &> /dev/null; then
+  yum install -y unzip
+fi
+
+# Extraire le zip dans /var/www/html (racine du site Apache)
+unzip -o /tmp/club.zip -d /var/www/html/
+
+# Modifier les permissions
+chown -R apache:apache /var/www/html
+chmod -R 755 /var/www/html
 
 echo "===== Redémarrage d'Apache ====="
 sudo systemctl restart httpd
