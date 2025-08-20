9619caa54f2a71896eba3e9f1d9bd61e44a0cc73
Aquilas <aquilas-damico.lengue-lengue-@externe.grdf.fr>
2025-08-21 01:43:40 +0200

    MàJ script deploy

--- dbconfig.php
+++ dbconfig.php
@@ -5,10 +5,37 @@
 	use Aws\SecretsManager\SecretsManagerClient;
 	use Aws\Exception\AwsException;
 
-	// Récupération depuis les variables d'environnement
+	function parse_properties($filename) {
+        $props = [];
+        $lines = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        foreach ($lines as $line) {
+            $line = trim($line);
+
+            // Ignorer les commentaires
+            if (empty($line) || $line[0] === '#' || $line[0] === ';') {
+                continue;
+            }
+
+            // Découper clé=valeur
+            $parts = explode("=", $line, 2);
+            if (count($parts) === 2) {
+                $key = trim($parts[0]);
+                $value = trim($parts[1]);
+                $props[$key] = $value;
+            }
+        }
+
+        return $props;
+    }
+
+    $config = parse_properties("param.properties");
+	/*// Récupération depuis les variables d'environnement
 	$secretName = getenv('AWS_SECRET_NAME') ?: 'default/secret/vault';
 	$region = getenv('AWS_REGION') ?: 'eu-west-1';
-
+	*/
+    $secretName = $config['AWS_SECRET_NAME'];
+    $region = $config['AWS_REGION'];
 	// Création du client Secrets Manager
 	$client = new SecretsManagerClient([
 	    'version' => '2017-10-17',
